302390a6342bfdfccbcda22410621b3b64ba1b1d
maciejkosiarski <maciek.kosiarski@gmail.com>
2017-07-07 20:17:45 +0100

    test action created

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
@@ -35,6 +36,7 @@ class AppKernel extends Kernel
         // kernel is a service that points to this class
         // optional 3rd argument is the route name
         $routes->add('/random/{limit}', 'kernel:randomAction');
+        $routes->add('/hello/{name}', 'kernel:helloAction', 'hello');
     }
 
     public function randomAction($limit)
@@ -43,6 +45,18 @@ class AppKernel extends Kernel
             'number' => rand(0, $limit)
         ));
     }
+    
+    public function helloAction($name)
+    {
+        $response = new Response(
+            '<h1>Hi, '.$name.' it is a micro kernel.</h2>',
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
+        );
+        
+        return $response;
+    }
+    
 }
 
 $kernel = new AppKernel('dev', true);
